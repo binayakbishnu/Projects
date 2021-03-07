797b2318cbbfcbf2fd83331a44939b49ca200385
Binayak Bishnu <71837066+BinayakBishnu@users.noreply.github.com>
2021-03-07 16:26:35 +0530

    Update Driver_Code.py

--- Driver_Code.py
+++ Driver_Code.py
@@ -1,2 +1,15 @@
 import Login
+import Database_login
+
+#Login.login()
+
+username = input("Enter username: ") #username
+result = Database_login.verifyusername(username)
+if result != 0:
+    password = input("Enter password: ") #password
+    result = Database_login.verifypassword(password)
+
+if result != 0:
+    print("Login successfull")
+    Login.login()
 
